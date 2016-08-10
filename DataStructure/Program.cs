fe2b92cb70c9a1b6f4fae6f221f853121319329a
GEZOO <574600361@qq.com>
2016-08-10 14:35:30 +0800

    编写FindMin和FindMax例程

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -14,25 +14,7 @@ namespace DataStructure
     {
         static void Main(string[] args)
         {
-            //MyLinkedList<int> myLinkedList = new MyLinkedList<int>();
-            //for (int i = 0; i < 100; i++)
-            //{
-            //    myLinkedList.Append(i);
-            //}
-
-            //foreach (var node in myLinkedList)
-            //{
-            //    Console.WriteLine(node);
-            //}
-            //Console.WriteLine(myLinkedList.Find(99).Item); 
-
-            Expression expression1 = Expression.Constant(3);
-            Console.WriteLine("NodeType:{0},Type:{1}",expression1.NodeType,expression1.Type);
-            Expression expression2 = Expression.Constant(6);
-            Console.WriteLine("NodeType:{0},Type:{1}",expression2.NodeType,expression2.Type);
-            Expression expression3 = Expression.Add(expression1,expression2);
-            Console.WriteLine("NodeType:{0},Type:{1}", expression3.NodeType, expression3.Type);
-            Console.WriteLine("add:{0}", expression3);
+            
             Console.ReadKey();
         }
 
---
+++ Tree/BinarySearchTree.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Tree
+{
+    public class BinarySearchTree<T> : IComparable<T>
+    {
+        private BinaryNode<T> Root;
+
+        public BinarySearchTree()
+        {
+            MakeEmpty();
+        }
+
+        public void MakeEmpty()
+        {
+            this.Root = null;
+        }
+        public bool IsEmpty()
+        {
+            return this.Root == null;
+        }
+        public bool Contains(T item) { throw new NotImplementedException(); }
+
+        public T FindMin()
+        {
+            if (IsEmpty()) throw new Exception("Root is null");
+            return FindMin(this.Root);
+        }
+        private T FindMin(BinaryNode<T> node)
+        {
+            if (node == null) return default(T);
+            if (node.Left == null) return node.Element;
+            return FindMin(node.Left);
+        }
+        public T FindMax()
+        {
+            if (IsEmpty()) throw new Exception("Root is null");
+            return FindMax(this.Root);
+        }
+
+        private T FindMax(BinaryNode<T> node)
+        {
+            //递归写法
+            if (node == null) return default(T);
+            if (node.Right == null) return node.Element;
+            return FindMax(node.Right);
+
+            //非递归写法
+            /*
+            if (node != null)
+                while (node.Right != null)
+                    node = node.Right;
+            return node.Element;
+             */
+        }
+        public void Insert(T item) { throw new NotImplementedException(); }
+        public void Remove(T item) { throw new NotImplementedException(); }
+        public void PrintTree() { throw new NotImplementedException(); }
+
+        public int CompareTo(T other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class BinaryNode<T>
+    {
+        public BinaryNode(T item) : this(item, null, null) { }
+
+        public BinaryNode(T item, BinaryNode<T> left, BinaryNode<T> right)
+        {
+            this.Element = item;
+            this.Left = left;
+            this.Right = right;
+        }
+
+        public T Element { get; set; }
+        public BinaryNode<T> Left { get; set; }
+        public BinaryNode<T> Right { get; set; }
+    }
+}
